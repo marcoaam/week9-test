b9f911313f4d3b439896bcd58078b1a1c215e611
marcoaam <marco.army@gmail.com>
2014-08-15 13:59:45 +0100

    swap keys and values

--- questions.rb
+++ questions.rb
@@ -124,6 +124,7 @@ end
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+
 end
 
 # get all the letters used in an array of words and return
@@ -138,6 +139,7 @@ end
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
+	hash.invert
 end
 
 # in a hash where the keys and values are all numbers
