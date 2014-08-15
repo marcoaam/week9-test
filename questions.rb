a3403fd8664f18a424918c2fcb38105f0003d309
marcoaam <marco.army@gmail.com>
2014-08-15 14:12:09 +0100

    Array to hash

--- questions.rb
+++ questions.rb
@@ -117,14 +117,14 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
-	array.slice(0..5)
+	array.take_while { |number| number <= 5 }
 end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
-
+	Hash[*array]
 end
 
 # get all the letters used in an array of words and return
@@ -146,6 +146,7 @@ end
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
+	hash.to_a.flatten.inject(:+)
 end
 
 # take out all the capital letters from a string
@@ -198,6 +199,7 @@ end
 # should return true for a 3 dot range like 1...20, false for a 
 # normal 2 dot range
 def is_a_3_dot_range?(range)
+
 end
 
 # get the square root of a number
